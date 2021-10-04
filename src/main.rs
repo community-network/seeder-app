3c03f09ba5b3a0d169dc3de114eeb541705f7778
Zefanja Jobse <22680656+zefanjajobse@users.noreply.github.com>
2021-10-04 19:16:11 +0200

    join-leave support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::process::Command;
+use std::process::{Child, Command};
 
-use futures_util::{future, pin_mut, StreamExt};
-use tokio::io::AsyncReadExt;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use tungstenite::{connect, Message};
+use url::Url;
 #[macro_use] extern crate serde_derive;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct BroadcastMessage {
-    r#type: String,
+    action: String,
     gameid: String,
 }
 
@@ -22,34 +21,25 @@ impl ::std::default::Default for SeederConfig {
     fn default() -> Self { Self { group_id: "0fda8e4c-5be3-11eb-b1da-cd4ff7dab605".into(), game_location: "C:\\Program Files (x86)\\Origin Games\\Battlefield 1\\bf1.exe".into() } }
 }
 
-
-#[tokio::main]
-async fn main() {
+fn main() {
+    let mut game: Option<Child> = None;
     let cfg: SeederConfig = confy::load_path("config.txt").unwrap();
     confy::store_path("config.txt", cfg.clone()).unwrap();
     let connect_addr = format!("ws://seeder.gametools.network:5252/ws/seeder?groupid={}", cfg.group_id);
+    let (mut socket, _response) =
+        connect(Url::parse(&connect_addr[..]).unwrap()).expect("Can't connect");
 
-    let url = url::Url::parse(&connect_addr).unwrap();
+    println!("Connected to the server with group id: {}", cfg.group_id);
+    println!("Using game in location: {}", cfg.game_location);
 
-    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx));
-
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    println!("WebSocket handshake has been successfully completed");
-
-    let (write, read) = ws_stream.split();
-
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap();
-			if matches!(data.clone(), Message::Text(_string)) {
-				let deserialized: BroadcastMessage = serde_json::from_str(&data.into_text().unwrap()[..]).unwrap();
-				let game = cfg.game_location.clone();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        if matches!(msg.clone(), Message::Text(_string)) {
+            let deserialized: BroadcastMessage = serde_json::from_str(&msg.into_text().unwrap()[..]).unwrap();
+            if &deserialized.action[..] == "joinServer" {
                 let game_id = &deserialized.gameid[..];
                 println!("joining id: {}", game_id);
-				let _command = Command::new(game).args([
+                game = Some(Command::new(cfg.game_location.clone()).args([
                     "-webMode",
                     "MP",
                     "-Origin_NoAppFocus",
@@ -63,26 +53,15 @@ async fn main() {
                     "-role",
                     "soldier",
                     "-asSpectator",
-                ]).spawn().unwrap();
-			}
-        })
-    };
-
-    pin_mut!(stdin_to_ws, ws_to_stdout);
-    future::select(stdin_to_ws, ws_to_stdout).await;
-}
-
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
+                ]).spawn().unwrap());
+            } else {
+                println!("Quitting game..");
+                match game {
+                    Some(ref mut process) => process.kill().unwrap(),
+                    None => println!("No game to quit!")
+                }
+            }
+        }
     }
+    // socket.close(None);
 }
