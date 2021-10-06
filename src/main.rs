77417086b45011e39481cd49bac1919a70068d7b
Zefanja Jobse <zefanjaj@gmail.com>
2021-10-06 16:41:00 +0200

    add anti-afk system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::{ffi::CString, mem, sync::{Arc, atomic}, thread::{self, sleep}, time::Duration};
 use tokio::process::Command;
 use futures_util::{future, pin_mut, StreamExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use winapi::um::winuser::{FindWindowA, INPUT, INPUT_KEYBOARD, KEYEVENTF_KEYUP, SendInput, SetForegroundWindow, ShowWindow};
 #[macro_use] extern crate serde_derive;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -23,6 +25,27 @@ impl ::std::default::Default for SeederConfig {
 // "ws://localhost:5051/ws/seeder?groupid={}"
 #[tokio::main]
 async fn main() {
+    let game_running = Arc::new(atomic::AtomicU32::new(0));
+    let game_running_clone = Arc::clone(&game_running);
+    // anti afk thread, runs when game is running
+    thread::spawn( move || {
+        loop {
+            if game_running_clone.load(atomic::Ordering::Relaxed) == 1 {
+                let window_name = CString::new("Battlefield™").unwrap();
+                unsafe {
+                    let window_handle = FindWindowA(std::ptr::null_mut(), window_name.as_ptr());
+                    SetForegroundWindow(window_handle);
+                    ShowWindow(window_handle, 9);
+                    sleep(Duration::from_millis(1808));
+                    key_enter(0x45);
+                    sleep(Duration::from_millis(100));
+                    ShowWindow(window_handle, 6);
+                    sleep(Duration::from_millis(120000));
+                }
+            }
+        }
+    });
+
     let cfg: SeederConfig = confy::load_path("config.txt").unwrap();
     confy::store_path("config.txt", cfg.clone()).unwrap();
     let connect_addr = format!("ws://seeder.gametools.network:5252/ws/seeder?groupid={}", cfg.group_id);
@@ -66,6 +89,8 @@ async fn main() {
                         Ok(_) => println!("game launched"),
                         Err(e) => println!("failed to launch game: {}", e)
                     }
+                    // game state == running game
+                    game_running.store(1, atomic::Ordering::Relaxed);
                 } else {
                     println!("Quitting game..");
                     let game_process = winproc::Process::from_name("bf1.exe");
@@ -78,6 +103,8 @@ async fn main() {
                         },
                         Err(_) => {println!("no game process found!");},
                     }
+                    // game state == no game
+                    game_running.store(0, atomic::Ordering::Relaxed);
                 }
 			}
             else if matches!(data.clone(), Message::Ping(_)) {
@@ -93,4 +120,29 @@ async fn main() {
 
     pin_mut!(stdin_to_ws, ws_to_stdout);
     future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+unsafe fn create_input(key_code: u16, flags: u32) -> INPUT {
+    let mut input = mem::zeroed::<INPUT>();
+    input.type_ = INPUT_KEYBOARD;
+    let mut ki = input.u.ki_mut();
+    ki.wVk = key_code;
+    ki.dwFlags = flags;
+    input
+}
+
+unsafe fn key_down(key_code: u16) {
+    let mut input = create_input(key_code, 0);
+    SendInput(1, &mut input, mem::size_of::<INPUT>() as i32);
+}
+
+unsafe fn key_up(key_code: u16) {
+    let mut input = create_input(key_code, KEYEVENTF_KEYUP);
+    SendInput(1, &mut input, mem::size_of::<INPUT>() as i32);
+}
+
+unsafe fn key_enter(key_code: u16) {
+    key_down(key_code);
+    sleep(Duration::from_millis(154));
+    key_up(key_code);
 }
