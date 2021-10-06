2066bada12650780909d3dba5b3b9016f2aab78a
Zefanja Jobse <zefanjaj@gmail.com>
2021-10-06 10:04:23 +0200

    ping/pong constandly to keep alive

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,16 @@ impl ::std::default::Default for SeederConfig {
     fn default() -> Self { Self { group_id: "0fda8e4c-5be3-11eb-b1da-cd4ff7dab605".into(), game_location: "C:\\Program Files (x86)\\Origin Games\\Battlefield 1\\bf1.exe".into() } }
 }
 
+// "ws://seeder.gametools.network:5252/ws/seeder?groupid={}
 #[tokio::main]
 async fn main() {
     let cfg: SeederConfig = confy::load_path("config.txt").unwrap();
     confy::store_path("config.txt", cfg.clone()).unwrap();
-    let connect_addr = format!("ws://seeder.gametools.network:5252/ws/seeder?groupid={}", cfg.group_id);
+    let connect_addr = format!("ws://localhost:5051/ws/seeder?groupid={}", cfg.group_id);
 
     let url = url::Url::parse(&connect_addr).unwrap();
 
-    let (_stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
@@ -79,6 +80,14 @@ async fn main() {
                     }
                 }
 			}
+            else if matches!(data.clone(), Message::Ping(_)) {
+                match stdin_tx.unbounded_send(Message::Pong(data.into_data())) {
+                    Ok(_) => {},
+                    Err(e) => println!("Failed to send pong: {}", e)
+                }
+            } else {
+                println!("{:#?}", data.clone());
+            }
         })
     };
 
